7ba4631af9207b34502765a9a1447aa64b3c56e4
FrankNemb <165398348+FrankNemb@users.noreply.github.com>
2024-06-05 19:04:05 +0200

    Update aluminium.js

--- scripts/data/aluminum/aluminium.js
+++ scripts/data/aluminum/aluminium.js
@@ -3,28 +3,28 @@ import { repairAndMaintenance, designAndConsultation, installationServices, manu
 export let aluminiumCards = [
     {
       title: "Design and Consultation",
-      image: "/assets/images/service/aluminium-frames/aluminium-design.jpeg",
+      image: "assets/images/service/aluminium-frames/aluminium-design.jpeg",
       text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
       icon: "/assets/icons/services/construction/properties.png",
       content: designAndConsultation
     },
     {
       title: "Manufacturing and Fabrication",
-      image: "/assets/images/service/aluminium-frames/aluminium-frame-manufacturing.webp",
+      image: "assets/images/service/aluminium-frames/aluminium-frame-manufacturing.webp",
       text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
       icon: "/assets/icons/services/construction/icons8-building-100.png",
       content: manufacturingAndFabrication
     },
     {
       title: "Installation Services",
-      image: "/assets/images/service/aluminium-frames/aluminium-installation.jpeg",
+      image: "assets/images/service/aluminium-frames/aluminium-installation.jpeg",
       text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
       icon: "/assets/icons/services/construction/icons8-house-with-a-garden-100.png",
       content: installationServices 
     },
     {
       title: "Repair and Maintenance",
-      image: "/assets/images/service/aluminium-frames/aluminum-repair.jpeg",
+      image: "assets/images/service/aluminium-frames/aluminum-repair.jpeg",
       text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
       icon: "/assets/icons/services/construction/icons8-clipboard-100.png",
       content: repairAndMaintenance
