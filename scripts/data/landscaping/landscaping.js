0f78980d6e54b18e9ac2ab60d75b23b1e4ce7973
FrankNemb <165398348+FrankNemb@users.noreply.github.com>
2024-06-06 23:27:54 +0200

    Update landscaping.js

--- scripts/data/landscaping/landscaping.js
+++ scripts/data/landscaping/landscaping.js
@@ -3,7 +3,7 @@ import { designAndPlanning, installationServices, maintenanceServices, specialty
 export let landscapingCards  = [
     {
         title: "Design and Planning",
-        image: "/assets/images/service/landscaping/landscaping-design.jpeg",
+        image: "/minec/assets/images/service/landscaping/landscaping-design.jpeg",
         text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
         icon: "/minec/images/icons/icons8-hammer-100.png",
         content: designAndPlanning
@@ -12,21 +12,21 @@ export let landscapingCards  = [
         title: "Installation Services",
         image: "/minec/assets/images/service/landscaping/landscaping-installation.webp",
         text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-        icon: "/minec/images/icons/icons8-hammer-100.png",
+        icon: "/minec/assets/images/icons/icons8-hammer-100.png",
         content: installationServices
     },
     {
         title: "Maintenance Services",
         image: "/minec/assets/images/service/landscaping/landscaping-maintenance.webp",
         text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-        icon: "/minec/images/icons/icons8-hammer-100.png",
+        icon: "/minec/assets/images/icons/icons8-hammer-100.png",
         content: maintenanceServices
     },
     {
         title: "Specialty Services",
         image: "/minec/assets/images/service/landscaping/irrigation.jpeg",
         text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-        icon: "/minec/images/icons/icons8-hammer-100.png",
+        icon: "/minec/assets/images/icons/icons8-hammer-100.png",
         content: specialtyServices
     },
 ]
