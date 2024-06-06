26968998226e7b082d7bccff9b7bb27098a777c1
FrankNemb <165398348+FrankNemb@users.noreply.github.com>
2024-06-06 18:43:10 +0200

    Update construction.js

--- scripts/data/construction/construction.js
+++ scripts/data/construction/construction.js
@@ -3,58 +3,58 @@ import { alterations, commercialBuilding, customHome, interiorAndExteriorFinishi
 export let constructionDevelopmentCards = [
   {
     title: "New Home Construction",
-    image: "/assets/images/service/construction/house-2.jpeg",
+    image: "/minec/assets/images/service/construction/house-2.jpeg",
     text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-    icon: "/assets/icons/services/construction/properties.png",
+    icon: "/minec/assets/icons/services/construction/properties.png",
     content: newHome
   },
   {
     title: "Commercial Construction",
-    image: "/assets/images/service/construction/commercial-building.jpeg",
+    image: "/minec//assets/images/service/construction/commercial-building.jpeg",
     text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-    icon: "/assets/icons/services/construction/icons8-building-100.png",
+    icon: "/minec/assets/icons/services/construction/icons8-building-100.png",
     content: commercialBuilding
   },
   {
     title: "Custom Home Building",
-    image: "/assets/images/service/construction/house-1.png",
+    image: "/minec/assets/images/service/construction/house-1.png",
     text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-    icon: "/assets/icons/services/construction/icons8-house-with-a-garden-100.png",
+    icon: "/minec/assets/icons/services/construction/icons8-house-with-a-garden-100.png",
     content: customHome 
   },
   {
     title: "Project Management",
-    image: "/assets/images/service/construction/project-management.jpeg",
+    image: "/minec/assets/images/service/construction/project-management.jpeg",
     text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
     icon: "/assets/icons/services/construction/icons8-clipboard-100.png",
     content: projectManagement
   },
   {
     title: "Interior and Exterior Finishing",
-    image: "/assets/images/service/construction/interior-and-exterior.webp",
+    image: "/minec/assets/images/service/construction/interior-and-exterior.webp",
     text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-    icon: "/assets/icons/services/construction/icons8-paint-brush-100.png",
+    icon: "/minec/assets/icons/services/construction/icons8-paint-brush-100.png",
     content: interiorAndExteriorFinishing
   },
   {
     title: "Additions and Expansions",
-    image: "/assets/images/service/construction/Construction-Site-2.jpeg",
+    image: "/minec/assets/images/service/construction/Construction-Site-2.jpeg",
     text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-    icon: "/assets/icons/services/construction/icons8-add-100.png",
+    icon: "/minec/assets/icons/services/construction/icons8-add-100.png",
     content: alterations
   },
   {
     title: "Site Preparation and Excavation",
-    image: "/assets/images/service/construction/excavation.jpeg",
+    image: "/minec/assets/images/service/construction/excavation.jpeg",
     text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-    icon: "/assets/icons/services/construction/icons8-bulldozer-100.png",
+    icon: "/minec/assets/icons/services/construction/icons8-bulldozer-100.png",
     content: sitePrepAndExcavations
   },
   {
     title: "Landscaping and Hardscaping",
-    image: "/assets/images/service/construction/lanscaping-garden.jpeg",
+    image: "/minec/assets/images/service/construction/lanscaping-garden.jpeg",
     text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-    icon: "/assets/icons/services/construction/icons8-oak-tree-100.png",
+    icon: "/minec/assets/icons/services/construction/icons8-oak-tree-100.png",
     content: landscapingAndHardscaping
   },
 ];
