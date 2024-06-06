91831c216283c527d41edf01f227c1d7b93ab872
FrankNemb <165398348+FrankNemb@users.noreply.github.com>
2024-06-06 21:30:32 +0200

    Update construction.js

--- scripts/data/construction/construction.js
+++ scripts/data/construction/construction.js
@@ -4,56 +4,56 @@ export let constructionDevelopmentCards = [
   {
     title: "New Home Construction",
     image: "/minec/assets/images/service/construction/house-2.jpeg",
-    text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
+    text: "Building Dreams, One Home at a Time",
     icon: "/minec/assets/icons/services/construction/properties.png",
     content: newHome
   },
   {
     title: "Commercial Construction",
     image: "/minec//assets/images/service/construction/commercial-building.jpeg",
-    text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
+    text: "Constructing Your Business Future",
     icon: "/minec/assets/icons/services/construction/icons8-building-100.png",
     content: commercialBuilding
   },
   {
     title: "Custom Home Building",
     image: "/minec/assets/images/service/construction/house-1.png",
-    text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
+    text: "Crafting Unique Homes to Your Specifications",
     icon: "/minec/assets/icons/services/construction/icons8-house-with-a-garden-100.png",
     content: customHome 
   },
   {
     title: "Project Management",
     image: "/minec/assets/images/service/construction/project-management.jpeg",
-    text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
+    text: "Managing Projects with Precision and Care",
     icon: "/minec/assets/icons/services/construction/icons8-clipboard-100.png",
     content: projectManagement
   },
   {
     title: "Interior and Exterior Finishing",
     image: "/minec/assets/images/service/construction/interior-and-exterior.webp",
-    text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
+    text: "Perfecting Every Detail, Inside and Out",
     icon: "/minec/assets/icons/services/construction/icons8-paint-brush-100.png",
     content: interiorAndExteriorFinishing
   },
   {
     title: "Additions and Expansions",
     image: "/minec/assets/images/service/construction/Construction-Site-2.jpeg",
-    text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
+    text: "Expanding Possibilities, Enhancing Homes",
     icon: "/minec/assets/icons/services/construction/icons8-add-100.png",
     content: alterations
   },
   {
     title: "Site Preparation and Excavation",
     image: "/minec/assets/images/service/construction/excavation.jpeg",
-    text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
+    text: "Preparing the Ground for Greatness",
     icon: "/minec/assets/icons/services/construction/icons8-bulldozer-100.png",
     content: sitePrepAndExcavations
   },
   {
     title: "Landscaping and Hardscaping",
     image: "/minec/assets/images/service/construction/lanscaping-garden.jpeg",
-    text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
+    text: "Creating Beautiful Outdoor Spaces",
     icon: "/minec/assets/icons/services/construction/icons8-oak-tree-100.png",
     content: landscapingAndHardscaping
   },
