b7d4d7f635fc1deda5c4d0ee48246cee4156d90d
FrankNemb <165398348+FrankNemb@users.noreply.github.com>
2024-06-07 00:05:56 +0200

    Update solar.js

--- scripts/data/solar/solar.js
+++ scripts/data/solar/solar.js
@@ -5,14 +5,14 @@ export let solarCards  = [
     {
         title: "Solar Panel Installation",
         image: "/minec/assets/images/service/solar/Solar-installation.png",
-        text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
+        text: "Harness the Power of the Sun",
         icon: "/minec/assets/icons/services/solar/icons8-sun-100.png",
         content: solarPanelInstallation
     },
     {
         title: "Solar Energy System Design",
         image: "/minec/assets/images/service/solar/solar-design.jpeg",
-        text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
+        text: "Customized Solar Solutions for Maximum Efficiency",
         icon: "/minec/assets/icons/services/solar/icons8-ruler-100.png",
         content: solarEnergySystemDesign
         
@@ -20,7 +20,7 @@ export let solarCards  = [
     {
         title: "Solar  Cleaning and Maintenance",
         image: "/minec/assets/images/service/solar/solar-panels-maintenance.webp",
-        text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
+        text: "Keeping Your Solar System at Peak Performance",
         icon: "/minec/assets/icons/services/solar/icons8-sponge-100.png",
         content: solarCleaningAndMaintenance
     },
@@ -28,7 +28,7 @@ export let solarCards  = [
     {
         title: "Battery Backup Solutions",
         image: "/minec/assets/images/service/solar/solar-battery-1.webp",
-        text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
+        text: "Uninterrupted Power with Reliable Battery Storage",
         icon: "/minec/assets/icons/services/solar/icons8-battery-100.png",
         content: batteryBackupSolutions
         
@@ -36,28 +36,28 @@ export let solarCards  = [
     {
         title: "Off-Grid Solar Solutions",
         image: "/minec/assets/images/service/solar/off-grid.webp",
-        text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
+        text: "Achieve Energy Independence Anywhere",
         icon: "/minec/assets/icons/services/solar/icons8-globe-100.png",
         content: offGridSolarSolutions
     },
     {
         title: "Grid-Tied Solar Systems",
         image: "/minec/assets/images/service/solar/grid-tied.jpeg",
-        text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
+        text: "The Best of Both Worlds: Solar Power and Grid Stability",
         icon: "/minec/assets/icons/services/solar/icons8-plug-100.png",
         content: gridTiedSolarSolutions
     },
     {
         title: "Solar Energy Consultation",
         image: "/minec/assets/images/service/solar/solar-consultation.jpeg",
-        text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
+        text: "Expert Advice for Your Solar Journey",
         icon: "/minec/assets/icons/services/solar/icons8-speech-bubble-100.png",
         content: solarEnergyConsultation
     },
     {
         title: "Solar Panel Repair Services",
         image: "/minec/assets/images/service/solar/solar-repair.jpeg",
-        text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
+        text: "Quick and Efficient Solar Panel Repairs",
         icon: "/minec/assets/icons/services/solar/icons8-wrench-100.png",
         content: solarPanelRepairServices
     }
