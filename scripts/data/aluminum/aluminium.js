b987d5d9be03574c5e826b8e9fd2575aabe65a0e
FrankNemb <165398348+FrankNemb@users.noreply.github.com>
2024-06-07 00:32:05 +0200

    Update aluminium.js

--- scripts/data/aluminum/aluminium.js
+++ scripts/data/aluminum/aluminium.js
@@ -4,28 +4,28 @@ export let aluminiumCards = [
     {
       title: "Design and Consultation",
       image: "/minec/assets/images/service/aluminium-frames/aluminium-design.jpeg",
-      text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
+      text: "Tailored Solutions for Unique Door, Window and Garage Door Designs",
       icon: "/minec/assets/icons/services/construction/properties.png",
       content: designAndConsultation
     },
     {
       title: "Manufacturing and Fabrication",
       image: "/minec/assets/images/service/aluminium-frames/aluminium-frame-manufacturing.webp",
-      text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
+      text: "Crafting Custom Windows and Doors for Your Unique Spaces",
       icon: "/minec/assets/icons/services/construction/icons8-building-100.png",
       content: manufacturingAndFabrication
     },
     {
       title: "Installation Services",
       image: "/minec/assets/images/service/aluminium-frames/aluminium-installation.jpeg",
-      text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
+      text: "Modernizing Your Space with High-Quality Aluminum",
       icon: "/minec/assets/icons/services/construction/icons8-house-with-a-garden-100.png",
       content: installationServices 
     },
     {
       title: "Repair and Maintenance",
       image: "/minec/assets/images/service/aluminium-frames/aluminum-repair.jpeg",
-      text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
+      text: "Keeping Your Doors and Windows in Top Condition",
       icon: "/minec/assets/icons/services/construction/icons8-clipboard-100.png",
       content: repairAndMaintenance
     }
