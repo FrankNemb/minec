883ad7766b6c46ddb3ed1d335751e7c707decf30
FrankNemb <165398348+FrankNemb@users.noreply.github.com>
2024-06-06 19:47:12 +0200

    Update electrical.js

--- scripts/data/electrical/electrical.js
+++ scripts/data/electrical/electrical.js
@@ -4,92 +4,92 @@ import { GeneratorInstallationAndMaintenance, ceilingFanInstallation, circuitBre
 export let electricalCards  = [
     {
         title: "Electrical System Installation",
-        image: "/assets/images/service/electrical/electrical-maintenance.jpeg",
+        image: "/minec/assets/images/service/electrical/electrical-maintenance.jpeg",
         text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-        icon: "/assets/icons/services/electrical/icons8-high-voltage-100.png",
+        icon: "/minec/assets/icons/services/electrical/icons8-high-voltage-100.png",
         content: electricalSystemInstallation
 
     },
     {
         title: "Electrical Repairs",
-        image: "/assets/images/service/electrical/specialty.jpeg",
+        image: "/minec/assets/images/service/electrical/specialty.jpeg",
         text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-        icon: "/assets/icons/services/electrical/icons8-wrench-100.png",
+        icon: "/minec/assets/icons/services/electrical/icons8-wrench-100.png",
         content: electricalRepairs
         
     },
     {
         title: "Lighting Installation and Upgrades",
-        image: "/assets/images/service/electrical/light-fixture-replacement.webp",
+        image: "/minec/assets/images/service/electrical/light-fixture-replacement.webp",
         text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-        icon: "/assets/icons/services/electrical/icons8-light-bulb-100.png",
+        icon: "/minec/assets/icons/services/electrical/icons8-light-bulb-100.png",
         content: lightingInstallationAndUpgrades
     },
     {
         title: "Circuit Breaker Panel Upgrades",
         image: "/assets/images/service/electrical/circuit-breaker.webp",
         text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-        icon: "/assets/icons/services/electrical/icons8-light-switch-100 copy.png",
+        icon: "/minec/assets/icons/services/electrical/icons8-light-switch-100 copy.png",
         content: circuitBreakerPanelUpgrades
     },
     {
         title: "Outlet and Switch Installation",
-        image: "/assets/images/service/electrical/outlet.jpeg",
+        image: "/minec/assets/images/service/electrical/outlet.jpeg",
         text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-        icon: "/assets/icons/services/electrical/icons8-plug-100.png",
+        icon: "/minec/assets/icons/services/electrical/icons8-plug-100.png",
         content: outletAndSwitchInstallation
 
     },
     {
         title: "Wiring and Rewiring",
-        image: "/assets/images/service/electrical/wiring.webp",
+        image: "/minec/assets/images/service/electrical/wiring.webp",
         text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-        icon: "/assets/icons/services/electrical/icons8-wiring-64.png",
+        icon: "/minec/assets/icons/services/electrical/icons8-wiring-64.png",
         content: wiringAndRewiring
         
     },
     {
         title: "Ceiling Fan Installation",
-        image: "/assets/images/service/electrical/ceiling-fan.webp",
+        image: "/minec/assets/images/service/electrical/ceiling-fan.webp",
         text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-        icon: "/assets/icons/services/electrical/icons8-fan-100.png",
+        icon: "/minec/assets/icons/services/electrical/icons8-fan-100.png",
         content: ceilingFanInstallation
     },
     {
         title: "Home Automation and Smart Home Systems",
-        image: "/assets/images/service/electrical/smart-home.png",
+        image: "/minec/assets/images/service/electrical/smart-home.png",
         text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-        icon: "/assets/icons/services/electrical/icons8-smart-home-100.png",
+        icon: "/minec/assets/icons/services/electrical/icons8-smart-home-100.png",
         content: homeAutomationAndSmartHomeSystems
     },
     {
         title: "Surge Protection",
-        image: "/assets/images/service/electrical/Surge-Protector.jpeg",
+        image: "/minec/assets/images/service/electrical/Surge-Protector.jpeg",
         text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-        icon: "/assets/icons/services/electrical/icons8-lightning-100.png",
+        icon: "/minec/assets/icons/services/electrical/icons8-lightning-100.png",
         content: surgeProtection
 
     },
     {
         title: "Electrical Safety Inspections",
-        image: "/assets/images/service/electrical/electrical-maintenance.jpeg",
+        image: "/minec/assets/images/service/electrical/electrical-maintenance.jpeg",
         text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-        icon: "/assets/icons/services/electrical/icons8-magnifier-100.png",
+        icon: "/minec/assets/icons/services/electrical/icons8-magnifier-100.png",
         content: electricalSafetyInspections
         
     },
     {
         title: "Generator Installation and Maintenance",
-        image: "/assets/images/service/electrical/generator-installation.jpeg",
+        image: "/minec/assets/images/service/electrical/generator-installation.jpeg",
         text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-        icon: "/assets/icons/services/electrical/icons8-battery-100.png",
+        icon: "/minec/assets/icons/services/electrical/icons8-battery-100.png",
         content: GeneratorInstallationAndMaintenance
     },
     {
         title: "Landscape Lighting",
-        image: "/assets/images/service/electrical/landscaping-lighting.jpeg",
+        image: "/minec/assets/images/service/electrical/landscaping-lighting.jpeg",
         text: "JHKJhsadjkhlfkja hasjkfa jhdjk hasjkldaj df",
-        icon: "/assets/icons/services/electrical/icons8-oak-tree-100.png",
+        icon: "/minec/assets/icons/services/electrical/icons8-oak-tree-100.png",
         content: landscapingLighting
     }
 ]
