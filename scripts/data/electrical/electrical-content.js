5ddfa6cd5a10e6a13a3abac3c8daf81add4636e0
FrankNemb <165398348+FrankNemb@users.noreply.github.com>
2024-06-06 20:33:56 +0200

    Update electrical-content.js

--- scripts/data/electrical/electrical-content.js
+++ scripts/data/electrical/electrical-content.js
@@ -1,68 +1,66 @@
 export let  electricalSystemInstallation =`
 
-Comprehensive Planning: Our team works closely with clients to design and implement electrical systems tailored to meet the specific requirements of residential, commercial, and industrial buildings. We ensure that every detail is meticulously planned to provide a robust and reliable electrical infrastructure.
-Installation: We handle the complete setup of electrical systems, ensuring proper and safe connections to the power grid. Our installation process is thorough and adheres to all industry standards and regulations to guarantee safety and efficiency.
-Testing and Commissioning: Before bringing the system online, we conduct rigorous testing to verify its safety and functionality. This includes checking all connections, verifying load capacities, and ensuring that everything operates seamlessly.
+<br><br><b>Comprehensive Planning:</b>  Our team works closely with clients to design and implement electrical systems tailored to meet the specific requirements of residential, commercial, and industrial buildings. We ensure that every detail is meticulously planned to provide a robust and reliable electrical infrastructure.
+<br><br><b>Installation:</b>  We handle the complete setup of electrical systems, ensuring proper and safe connections to the power grid. Our installation process is thorough and adheres to all industry standards and regulations to guarantee safety and efficiency.
+<br><br><b>Testing and Commissioning: </b> Before bringing the system online, we conduct rigorous testing to verify its safety and functionality. This includes checking all connections, verifying load capacities, and ensuring that everything operates seamlessly.
 `;
 
 export let  electricalRepairs =`
-Diagnosis: Our expert electricians are skilled in identifying electrical issues through detailed inspections and troubleshooting. We use advanced diagnostic tools to pinpoint problems quickly and accurately.
-Repair Services: We provide comprehensive repair services, fixing faulty wiring, malfunctioning devices, and other electrical problems. Our goal is to restore full functionality and safety to your electrical systems.
-Preventive Maintenance: To prevent future issues, we offer regular maintenance services that ensure your electrical systems remain reliable. This includes checking connections, inspecting components, and performing necessary adjustments or replacements.
+<br><br><b>Diagnosis:</b>  Our expert electricians are skilled in identifying electrical issues through detailed inspections and troubleshooting. We use advanced diagnostic tools to pinpoint problems quickly and accurately.
+<br><br><b>Repair Services: </b> We provide comprehensive repair services, fixing faulty wiring, malfunctioning devices, and other electrical problems. Our goal is to restore full functionality and safety to your electrical systems.
+<br><br><b>Preventive Maintenance: </b> To prevent future issues, we offer regular maintenance services that ensure your electrical systems remain reliable. This includes checking connections, inspecting components, and performing necessary adjustments or replacements.
 `;
 
 export let  lightingInstallationAndUpgrades =`
 
-Design and Planning: We work with clients to create custom lighting plans that enhance both the aesthetics and functionality of their spaces. Our designs incorporate the latest lighting technologies and trends to provide optimal illumination.
-Installation: Our team installs new lighting fixtures, including energy-efficient LED lights, chandeliers, recessed lighting, and more. We ensure that all installations are done safely and correctly.
-Upgrades: If you have outdated or inefficient lighting systems, we offer upgrade services to replace them with modern, energy-saving options. This not only improves lighting quality but also reduces energy costs.
-
-
+<br><br><b>Design and Planning: </b> We work with clients to create custom lighting plans that enhance both the aesthetics and functionality of their spaces. Our designs incorporate the latest lighting technologies and trends to provide optimal illumination.
+<br><br><b>Installation:</b>  Our team installs new lighting fixtures, including energy-efficient LED lights, chandeliers, recessed lighting, and more. We ensure that all installations are done safely and correctly.
+<br><br><b>Upgrades: </b> If you have outdated or inefficient lighting systems, we offer upgrade services to replace them with modern, energy-saving options. This not only improves lighting quality but also reduces energy costs.
 `;
 
 export let  circuitBreakerPanelUpgrades =`
-Design and Planning: We work with clients to create custom lighting plans that enhance both the aesthetics and functionality of their spaces. Our designs incorporate the latest lighting technologies and trends to provide optimal illumination.
-Installation: Our team installs new lighting fixtures, including energy-efficient LED lights, chandeliers, recessed lighting, and more. We ensure that all installations are done safely and correctly.
-Upgrades: If you have outdated or inefficient lighting systems, we offer upgrade services to replace them with modern, energy-saving options. This not only improves lighting quality but also reduces energy costs.
+<br><br><b>Design and Planning:</b>  We work with clients to create custom lighting plans that enhance both the aesthetics and functionality of their spaces. Our designs incorporate the latest lighting technologies and trends to provide optimal illumination.
+<br><br><b>Installation: </b> Our team installs new lighting fixtures, including energy-efficient LED lights, chandeliers, recessed lighting, and more. We ensure that all installations are done safely and correctly.
+<br><br><b>Upgrades: </b> If you have outdated or inefficient lighting systems, we offer upgrade services to replace them with modern, energy-saving options. This not only improves lighting quality but also reduces energy costs.
 `;
 export let  outletAndSwitchInstallation =`
-New Installations: We add new outlets and switches where needed, ensuring they are placed conveniently and comply with electrical codes. This enhances the functionality and convenience of your spaces.
-Upgrades: Our team can replace old or damaged outlets and switches with modern, safer versions. We offer options like GFCI outlets for added protection and USB outlets for charging devices.
-Specialty Outlets: We also install specialized outlets, such as weatherproof outdoor outlets and dedicated circuits for high-demand appliances, to meet specific needs.
+<br><br><b>New Installations: </b> We add new outlets and switches where needed, ensuring they are placed conveniently and comply with electrical codes. This enhances the functionality and convenience of your spaces.
+<br><br><b>Upgrades: </b> Our team can replace old or damaged outlets and switches with modern, safer versions. We offer options like GFCI outlets for added protection and USB outlets for charging devices.
+<br><br><b>Specialty Outlets:</b>  We also install specialized outlets, such as weatherproof outdoor outlets and dedicated circuits for high-demand appliances, to meet specific needs.
 `;
 export let  wiringAndRewiring =`
-Initial Wiring: For new constructions, we install wiring systems from scratch, ensuring all connections are secure and compliant with building codes. We use high-quality materials to ensure long-lasting performance.
-Rewiring: In older buildings, we update outdated or unsafe wiring systems to meet modern standards. This improves safety, efficiency, and the ability to support new technologies.
-Hidden Wiring: To maintain the aesthetic appeal of your interiors, we offer concealed wiring solutions that minimize visible wires and maintain a clean look.
+<br><br><b>Initial Wiring: </b> For new constructions, we install wiring systems from scratch, ensuring all connections are secure and compliant with building codes. We use high-quality materials to ensure long-lasting performance.
+<br><br><b>Rewiring: </b> In older buildings, we update outdated or unsafe wiring systems to meet modern standards. This improves safety, efficiency, and the ability to support new technologies.
+<br><br><b>Hidden Wiring: </b> To maintain the aesthetic appeal of your interiors, we offer concealed wiring solutions that minimize visible wires and maintain a clean look.
 `;
 export let  ceilingFanInstallation =`
-Selection Assistance: We help clients choose the right ceiling fan for their needs, considering factors like room size, ceiling height, and design preferences. Our recommendations ensure optimal air circulation and aesthetic harmony.
-Installation: Our technicians securely install ceiling fans, ensuring they are properly balanced and wired correctly. We handle all aspects of the installation, including mounting and electrical connections.
-Maintenance: To ensure your ceiling fan operates efficiently and quietly, we offer maintenance services such as cleaning, lubrication, and adjustments.
+<br><br><b>Selection Assistance: </b> We help clients choose the right ceiling fan for their needs, considering factors like room size, ceiling height, and design preferences. Our recommendations ensure optimal air circulation and aesthetic harmony.
+<br><br><b>Installation: </b> Our technicians securely install ceiling fans, ensuring they are properly balanced and wired correctly. We handle all aspects of the installation, including mounting and electrical connections.
+<br><br><b>Maintenance: </b> To ensure your ceiling fan operates efficiently and quietly, we offer maintenance services such as cleaning, lubrication, and adjustments.
 `;
 export let  homeAutomationAndSmartHomeSystems =`
-Consultation and Design: We advise clients on the latest smart home technologies and design integrated systems that enhance convenience, security, and energy efficiency. Our solutions are tailored to meet your specific needs and preferences.
-Installation: Our team sets up smart home devices, including lighting controls, security systems, thermostats, and more. We ensure that all devices are installed correctly and configured for optimal performance.
-Integration: We ensure all smart home devices work together seamlessly, providing a cohesive and intuitive user experience. Our integration services make it easy to control your home through a central hub or smartphone app.
+<br><br><b>Consultation and Design: </b> We advise clients on the latest smart home technologies and design integrated systems that enhance convenience, security, and energy efficiency. Our solutions are tailored to meet your specific needs and preferences.
+<br><br><b>Installation:</b>  Our team sets up smart home devices, including lighting controls, security systems, thermostats, and more. We ensure that all devices are installed correctly and configured for optimal performance.
+<br><br><b>Integration:</b>  We ensure all smart home devices work together seamlessly, providing a cohesive and intuitive user experience. Our integration services make it easy to control your home through a central hub or smartphone app.
 `;
 export let  surgeProtection =`
-Assessment: We evaluate your property’s current surge protection measures and identify areas for improvement. This includes checking for adequate grounding and existing surge protectors.
-Installation: We install whole-house surge protectors to safeguard your electrical systems and appliances from power surges caused by lightning, grid fluctuations, and other sources.
-Maintenance: Regular inspections and maintenance ensure that your surge protection systems remain effective and ready to protect your valuable electronics.
+<br><br><b>Assessment: </b> We evaluate your property’s current surge protection measures and identify areas for improvement. This includes checking for adequate grounding and existing surge protectors.
+<br><br><b>Installation: </b> We install whole-house surge protectors to safeguard your electrical systems and appliances from power surges caused by lightning, grid fluctuations, and other sources.
+<br><br><b>Maintenance: </b> Regular inspections and maintenance ensure that your surge protection systems remain effective and ready to protect your valuable electronics.
 `;
 export let  electricalSafetyInspections =`
-Comprehensive Inspections: We perform detailed inspections of your electrical systems to identify potential hazards such as outdated wiring, overloaded circuits, and faulty components. Our inspections are thorough and adhere to safety standards.
-Safety Reports: After the inspection, we provide comprehensive reports with findings and recommendations for improving safety. This helps you address any issues before they become serious problems.
-Code Compliance: We ensure that all electrical work complies with local and national safety codes, providing peace of mind that your electrical system is safe and up to standard.
+<br><br><b>Comprehensive Inspections: </b> We perform detailed inspections of your electrical systems to identify potential hazards such as outdated wiring, overloaded circuits, and faulty components. Our inspections are thorough and adhere to safety standards.
+<br><br><b>Safety Reports:</b>  After the inspection, we provide comprehensive reports with findings and recommendations for improving safety. This helps you address any issues before they become serious problems.
+<br><br><b>Code Compliance:</b>  We ensure that all electrical work complies with local and national safety codes, providing peace of mind that your electrical system is safe and up to standard.
 `;
 export let  GeneratorInstallationAndMaintenance =`
-Selection and Consultation: We help clients choose the right generator for their power needs, considering factors such as power capacity, fuel type, and budget. Our recommendations ensure reliable backup power in case of outages.
-Installation: We handle the complete installation of standby generators, ensuring they are properly connected and ready for use. This includes setting up automatic transfer switches for seamless power transition.
-Maintenance: Regular maintenance services, including testing, oil changes, and part replacements, ensure that your generator remains in optimal condition and ready to provide power when needed.
+<br><br><b>Selection and Consultation:</b>  We help clients choose the right generator for their power needs, considering factors such as power capacity, fuel type, and budget. Our recommendations ensure reliable backup power in case of outages.
+<br><br><b>Installation: </b> We handle the complete installation of standby generators, ensuring they are properly connected and ready for use. This includes setting up automatic transfer switches for seamless power transition.
+<br><br><b>Maintenance: </b> Regular maintenance services, including testing, oil changes, and part replacements, ensure that your generator remains in optimal condition and ready to provide power when needed.
 `;
 export let  landscapingLighting =`
-Design: We create custom landscape lighting plans that highlight key features of your outdoor spaces and enhance their beauty. Our designs incorporate energy-efficient fixtures and strategic placement for maximum effect.
-Installation: Our team installs landscape lighting fixtures, ensuring they are weatherproof and correctly wired for safe operation. We handle all aspects of installation, from trenching for wiring to setting up control systems.
-Upgrades and Maintenance: We offer upgrade services to replace outdated lighting with more efficient options and provide ongoing maintenance to keep your landscape lighting in top condition. This includes cleaning fixtures, replacing bulbs, and adjusting settings for optimal performance.
+<br><br><b>Design:</b>  We create custom landscape lighting plans that highlight key features of your outdoor spaces and enhance their beauty. Our designs incorporate energy-efficient fixtures and strategic placement for maximum effect.
+<br><br><b>Installation:</b>  Our team installs landscape lighting fixtures, ensuring they are weatherproof and correctly wired for safe operation. We handle all aspects of installation, from trenching for wiring to setting up control systems.
+<br><br><b>Upgrades and Maintenance:</b> We offer upgrade services to replace outdated lighting with more efficient options and provide ongoing maintenance to keep your landscape lighting in top condition. This includes cleaning fixtures, replacing bulbs, and adjusting settings for optimal performance.
 
 `;
