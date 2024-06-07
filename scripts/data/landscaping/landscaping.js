5e3d00910211501ca9d8954eee24dc28832f31b9
FrankNemb <165398348+FrankNemb@users.noreply.github.com>
2024-06-07 16:40:25 +0200

    Update landscaping.js

--- scripts/data/landscaping/landscaping.js
+++ scripts/data/landscaping/landscaping.js
@@ -5,28 +5,28 @@ export let landscapingCards  = [
         title: "Design and Planning",
         image: "/minec/assets/images/service/landscaping/landscaping-design.jpeg",
         text: "Crafting Beautiful Outdoor Spaces for Your Home",
-        icon: "/minec/assets/icons/services/icons8-hammer-100.png",
+        icon: "/minec/assets/icons/services/landscaping/icons8-design-100.png",
         content: designAndPlanning
     },
     {
         title: "Installation Services",
         image: "/minec/assets/images/service/landscaping/landscaping-installation.webp",
         text: "Bringing Life and Color to Your Garden",
-        icon: "/minec/assets/images/icons/icons8-hammer-100.png",
+        icon: "/minec/assets/icons/services/landscaping/icons8-flower-100.png",
         content: installationServices
     },
     {
         title: "Maintenance Services",
         image: "/minec/assets/images/service/landscaping/landscaping-maintenance.webp",
         text: "Greening Your Lawn with Expert Care",
-        icon: "/minec/assets/images/icons/icons8-hammer-100.png",
+        icon: "/minec/assets/icons/services/landscaping/icons8-shovel-100.png",
         content: maintenanceServices
     },
     {
         title: "Irrigation Installation and Maintenance",
         image: "/minec/assets/images/service/landscaping/irrigation.jpeg",
         text: "Efficient Watering Solutions for Your Landscape",
-        icon: "/minec/assets/icons/services/icons8-hammer-100.png",
+        icon: "/minec/assets/icons/services/landscaping/icons8-drop-100.png",
         content: specialtyServices
     },
 ]
